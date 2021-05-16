92b2f256568e16cdd2f485a9e912fe10caff1670
rasyidf <ufarasfa@gmail.com>
2021-05-16 16:54:22 +0700

    Initial Commit

---
+++ pages/report/audit_supplier.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Supplier</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/report/audit_user.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>User Audit</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
