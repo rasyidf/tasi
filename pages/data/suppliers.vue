1bafad47f50423f6282cdef068437074028a146c
rasyidf <ufarasfa@gmail.com>
2021-05-18 15:31:47 +0700

    Add Suppliers

--- pages/data/suppliers.vue
+++ pages/data/suppliers.vue
@@ -1,35 +1,55 @@
 <template>
   <div class="container">
-    <el-card>
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column label="Date" width="180">
+    <el-card class="p-0">
+      <el-table v-loading="isLoading" :data="products" style="width: 100%">
+        <el-table-column label="Date" width="150">
           <template slot-scope="scope">
             <i class="el-icon-time"></i>
-            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+            <span style="margin-left: 10px">{{ scope.row.ModifiedDate }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="Name" width="180">
+        <el-table-column label="Name">
           <template slot-scope="scope">
-            <el-popover trigger="hover" placement="top">
-              <p>Name: {{ scope.row.name }}</p>
-              <p>Addr: {{ scope.row.address }}</p>
-              <div slot="reference" class="name-wrapper">
-                <el-tag size="medium">{{ scope.row.name }}</el-tag>
-              </div>
-            </el-popover>
+            {{ scope.row.Name }}
           </template>
         </el-table-column>
-        <el-table-column label="Operations">
+        <el-table-column label="Address">
           <template slot-scope="scope">
-            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-              >Edit</el-button
+            {{ scope.row.Address }}
+          </template>
+        </el-table-column>
+        <el-table-column fixed="right" width="128">
+          <template slot="header" slot-scope="scope">
+            <el-button
+              type="primary"
+              size="mini"
+              icon="el-icon-plus"
+              @click="handleAdd(scope.$index, scope.row)"
             >
+              Add Item
+            </el-button>
+          </template>
+          <template slot-scope="scope">
             <el-button
               size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)"
-              >Delete</el-button
+              icon="el-icon-edit"
+              @click="handleEdit(scope.$index, scope.row)"
+            ></el-button>
+            <el-popconfirm
+              confirm-button-text="OK"
+              cancel-button-text="No, Thanks"
+              icon="el-icon-info"
+              icon-color="red"
+              title="Are you sure to delete this?"
+              @confirm="handleDelete(scope.$index, scope.row)"
             >
+              <el-button
+                slot="reference"
+                size="mini"
+                icon="el-icon-delete"
+                type="danger"
+              ></el-button>
+            </el-popconfirm>
           </template>
         </el-table-column>
       </el-table>
@@ -38,39 +58,40 @@
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex'
+
 export default {
-  data() {
-    return {
-      tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-      ],
-    }
+  computed: {
+    ...mapGetters('suppliers', {
+      suppliers: 'list',
+      isLoading: 'isLoading',
+    }),
+    ...mapState([
+      'route', // vuex-router-sync
+    ]),
+  },
+
+  async created() {
+    await this.$store.dispatch('suppliers/fetchList')
   },
   methods: {
     handleEdit(index, row) {
-      console.log(index, row)
+      this.$message({
+        message: 'Edited.',
+        type: 'success',
+      })
     },
     handleDelete(index, row) {
-      console.log(index, row)
+      this.$message({
+        message: 'Deleted.',
+        type: 'success',
+      })
+    },
+    handleAdd(index, row) {
+      this.$message({
+        message: 'Added.',
+        type: 'success',
+      })
     },
   },
 }
