fa6bf13dc95b01af80299c61247d72c83d06f312
rasyidf <ufarasfa@gmail.com>
2021-06-06 03:17:56 +0700

    Tidy Up

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,9 @@
       <Navbar />
     </el-header>
     <el-container>
-      <el-main class="main-item"> <Nuxt /></el-main>
+      <el-main class="main-item">
+        <transition name="el-fade-in-linear"> <Nuxt /></transition>
+      </el-main>
       <el-footer class="main-footer">
         <span> Copyright &copy; 2021, UNPAK</span>
       </el-footer>
